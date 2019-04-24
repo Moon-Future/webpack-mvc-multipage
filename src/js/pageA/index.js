6c1430d7466ac24fcfa19f519b3668eba7b4bccf
Leo Chan <236338364@qq.com>
2019-04-25 01:28:24 +0800

    ..

---
+++ src/js/pageA/index.js
@@ -0,0 +1,1 @@
+console.log('pageA index')
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,32 +4,60 @@ const OptimizeCss = require('optimize-css-assets-webpack-plugin') // 压缩css
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extracts CSS into separate files
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const pages = require('./config/entry.config.js')
-const rimraf = require('rimraf')
+const watch = require('watch')
 
-rimraf.sync('./dist', fs, function cb() {
-  console.log('dist目录已清空')
+function createTemplate(content, main = './src/_main.ejs') {
+  let strContent = fs.readFileSync( './'+ content, 'utf-8')
+  let strMain = fs.readFileSync(main, 'utf-8')
+  let template = content.split('\\')[content.split('\\').length - 1].split('.')[0];
+  strMain = strMain.replace(/<%= htmlWebpackPlugin.options.content %>/, strContent)
+  fs.writeFileSync(path.join(__dirname, `./src/template/template_${template}.ejs`), strMain)
+  return path.join(__dirname, `./src/template/template_${template}.ejs`)
+}
+
+watch.watchTree('./src/project', function (f, curr, prev) {
+  if (typeof f == "object" && prev === null && curr === null) {
+    // Finished walking the tree
+  } else if (prev === null) {
+    // f is a new file
+    createTemplate(f);
+  } else if (curr.nlink === 0) {
+    // f was removed
+  } else {
+    console.log('f', f)
+    // f was changed
+    createTemplate(f);
+  }
 })
 
-
 module.exports = {
+  devServer: {
+    port: 3000,
+    progress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true
+  },
+
   optimization: { // 优化项
     minimizer: [
       new UglifyJsPlugin({
         cache: true,
         parallel: true,
-        sourceMap: false
+        sourceMap: true
       }),
       new OptimizeCss()
     ]
   },
 
-  mode: 'production', // production、development
+  mode: 'development', // production、development
 
   entry: pages.entry,
 
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
+    // publicPath: '//ajs.lotpure.cn'
   },
 
   resolve: {
